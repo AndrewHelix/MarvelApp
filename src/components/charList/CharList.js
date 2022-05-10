80cd67ab76f02bde6ef92049d9137c3ded514ed5
AndrewHelix <andreyshulga95@gmail.com>
2022-05-10 23:29:26 +0300

    add focus by ref in charlist

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,9 +1,10 @@
 import { Component } from "react/cjs/react.development";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import MarvelService from "../../services/MarvelService";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Spinner from "../spinner/Spinner";
 import "./charList.scss";
+import { render } from "@testing-library/react";
 
 class CharList extends Component {
   state = {
@@ -67,7 +68,8 @@ class CharList extends Component {
   }
 
   render() {
-    const { error, loading, chars, newItemLoading, offset, charEnded } = this.state;
+    const { error, loading, chars, newItemLoading, offset, charEnded } =
+      this.state;
 
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
@@ -78,11 +80,12 @@ class CharList extends Component {
         {errorMessage}
         {spinner}
         {content}
-        <button 
+        <button
           className="button button__main button__long"
           disabled={newItemLoading}
-          style={{display: charEnded ? 'none' : 'block'}}
-          onClick={() => this.onRequest(offset)}>
+          style={{ display: charEnded ? "none" : "block" }}
+          onClick={() => this.onRequest(offset)}
+        >
           <div className="inner">load more</div>
         </button>
       </div>
@@ -90,24 +93,50 @@ class CharList extends Component {
   }
 }
 
-const Char = ({ char: { name, thumbnail }, id, onCharSelected }) => {
-  const objectFitStyle = thumbnail.includes("image_not_available")
+class Char extends Component {
+  objectFitStyle = this.props.char.thumbnail.includes("image_not_available")
     ? "contain"
     : "unset";
-  const imgStyles = {
-    objectFit: objectFitStyle,
+  imgStyles = {
+    objectFit: this.objectFitStyle,
   };
 
-  return (
-    <li className="char__item" id={id} onClick={() => onCharSelected(id)}>
-      <img src={thumbnail} alt={name} style={imgStyles} />
-      <div className="char__name">{name}</div>
-    </li>
-  );
-};
+  setCharRef = (elem) => {
+    this.charRef = elem;
+  }
+
+  setFocus = (char) => {
+    char.classList.add('char__item_selected')
+    char.focus()
+  }
+
+  render() {
+    const {
+      char: { name, thumbnail },
+      id,
+      onCharSelected,
+    } = this.props;
+
+    return (
+      <li
+        className="char__item"
+        id={id}
+        ref={this.setCharRef}
+        onClick={() => {
+          onCharSelected(id)
+          this.setFocus(this.charRef)}}
+        tabIndex={0}
+        onBlur={() => {this.charRef.classList.remove('char__item_selected')}}
+      >
+        <img src={thumbnail} alt={name} style={this.imgStyles} />
+        <div className="char__name">{name}</div>
+      </li>
+    );
+  }
+}
 
 CharList.propTypes = {
-  onCharSelected: PropTypes.func.isRequired
-}
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
